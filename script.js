f213a93a0576404ea76878e7ef3827f6d50a8bfb
alex-s4 <al39726867@gmail.com>
2023-02-16 10:41:27 +0800

    When the user accepts the cookie policy, hide the fixed message

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 var navCity = document.querySelectorAll(".city-name")
+var cookieDialog = document.getElementById("cookie-dialog")
+var cookieAcceptBtn = document.getElementById("confirm-cookie-btn")
+
+// When a user clicks on a city, alert the message "Loading weather report..."
 
 for (var i=0; i<navCity.length; i++){
     navCity[i].onclick = function() {
         alert("Loading weather report...")
     }
+}
+
+// When the user accepts the cookie policy, hide the fixed message
+cookieAcceptBtn.onclick = function() {
+    cookieDialog.style.visibility = "hidden"
 }
